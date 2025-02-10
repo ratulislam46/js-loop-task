2399fff2f6272189ddf110f38492bbb14c85fad8
ratulislam46 <arfanratul46@gmail.com>
2025-02-11 03:05:29 +0600

    commitment done

--- commitment.js
+++ commitment.js
@@ -1,2 +1,11 @@
-for(i = 1; i <= 60; i++)
-    console.log(i, 'I will invest at least 6 hrs every single day for next 60 days!');
+//use for loop
+// for(i = 1; i <= 60; i++)
+//     console.log(i, 'I will invest at least 6 hrs every single day for next 60 days!');
+
+
+//use while loop
+let i = 1;
+while(i <=60){
+    console.log(i,'I will invest at least 6 hrs every single day for next 60 days!')
+    i++;
+}
